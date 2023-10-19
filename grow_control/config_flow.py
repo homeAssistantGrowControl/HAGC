1125fc8332cffd085ff3dc46c7bc104b39419c58
n1m4nd1 <mk100@outlook.de>
2023-10-19 23:29:00 +0200

    init

--- grow_control/config_flow.py
+++ grow_control/config_flow.py
@@ -1,13 +1,15 @@
 import voluptuous as vol
+from datetime import timedelta
 from homeassistant import config_entries
 from homeassistant.core import callback
 from homeassistant.helpers import config_entry_flow
 from homeassistant.util import dt as dt_util
+from homeassistant.const import SERVICE_TURN_ON
 
 class GrowControlConfigFlow(config_entries.ConfigFlow, domain="grow_control"):
     async def async_step_user(self, user_input=None):
         errors = {}
-        entities = self.hass.states.async_entity_ids()
+        entities = self.hass.states.async_entity_ids("calendar")
 
         if user_input is not None:
             # Validate and store the user's input
@@ -18,6 +20,8 @@ class GrowControlConfigFlow(config_entries.ConfigFlow, domain="grow_control"):
             if not user_input["end_date"]:
                 errors["end_date"] = "empty_end_date"
             else:
+                # Create events based on user input
+                await self.create_calendar_events(user_input)
                 return self.async_create_entry(
                     title="Grow Control",
                     data=user_input
@@ -32,6 +36,27 @@ class GrowControlConfigFlow(config_entries.ConfigFlow, domain="grow_control"):
             }),
             errors=errors,
         )
+
+    async def create_calendar_events(self, user_input):
+        # Create events in the selected calendar entity
+        calendar_entity_id = user_input["calendar_entity_id"]
+        start_date: datetime | None = dt_util.parse_datetime(user_input["start_date"])
+        end_date = dt_util.parse_datetime(user_input["end_date"])
+
+        # Create events from start_date to end_date
+        while start_date <= end_date:
+            await self.hass.services.async_call(
+                "calendar",
+                "event",
+                {
+                    "calendar_id": calendar_entity_id,
+                    "title": "Your Event Title",
+                    "start": start_date.isoformat(),
+                    "end": (start_date + timedelta(days=1)).isoformat(),
+                },
+                blocking=True
+            )
+            start_date += timedelta(days=1)
 
     @staticmethod
     @callback
